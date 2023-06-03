2787cc62622a5635bf281724bb8c18e61941b012
Agus Tinus Turnip <agus.tinus@mhs.itenas.ac.id>
2023-06-03 20:15:24 +0700

    Setup project

---
+++ app/src/main/java/com/dicoding/catidentifierapp/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.dicoding.catidentifierapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.dicoding.catidentifierapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
